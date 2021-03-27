ee12d7fc1ea91ebd4e4caacd66b3bd6136c1e37a
Naveen.S <naveensagayaselvaraj@gmail.com>
2021-03-27 17:33:30 +0530

    webdriver-manager code updated

--- conftest.py
+++ conftest.py
@@ -2,13 +2,14 @@ from selenium import webdriver
 import pytest
 from pages.homepage import HomePage
 from pages.loginpage import LoginPage
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="session")
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument('ignore-certificate-errors')
-    driver_ = webdriver.Chrome(r'D:/Naveen/Selenium/chromedriver_win32/chromedriver.exe',chrome_options=options)
+    driver_ = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options)
     driver_.maximize_window()
     driver_.get("http://zero.webappsecurity.com/index.html")
     return driver_
